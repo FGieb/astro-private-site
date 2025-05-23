062de18bbb75992a884002b95bd9dde64569e7fe
FGieb <79575819+FGieb@users.noreply.github.com>
2025-05-23 11:37:13 +0200

    Update getGallery.js

--- netlify/functions/getGallery.js
+++ netlify/functions/getGallery.js
@@ -1,1 +1,49 @@
+const axios = require("axios");
+const crypto = require("crypto");
+
+exports.handler = async function () {
+  const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
+  const apiKey = process.env.CLOUDINARY_API_KEY;
+  const apiSecret = process.env.CLOUDINARY_API_SECRET;
+
+  const folder = "daily-images";
+  const timestamp = Math.floor(Date.now() / 1000);
+  const expression = `resource_type:image OR resource_type:video AND folder=${folder}`;
+  const signature = crypto
+    .createHash("sha1")
+    .update(`expression=${expression}&timestamp=${timestamp}${apiSecret}`)
+    .digest("hex");
+
+  const url = `https://api.cloudinary.com/v1_1/${cloudName}/resources/search`;
+
+  try {
+    const res = await axios.post(
+      url,
+      {
+        expression,
+        timestamp,
+        api_key: apiKey,
+        signature,
+      }
+    );
+
+    const images = res.data.resources.map((file) => {
+      if (file.resource_type === "video") {
+        return `https://res.cloudinary.com/${cloudName}/video/upload/so_0/${file.public_id}.jpg`;
+      } else {
+        return file.secure_url;
+      }
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(images),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
 
