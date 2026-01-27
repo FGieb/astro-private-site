bff84bda914c7e7ae5e3fd12013dff9f5ef82b4d
FGieb <79575819+FGieb@users.noreply.github.com>
2026-01-27 10:08:28 +0100

    Update calendar-save.js

--- netlify/functions/calendar-save.js
+++ netlify/functions/calendar-save.js
@@ -1,1 +1,21 @@
+import { getStore } from "@netlify/blobs";
+
+export default async (req) => {
+  if (req.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed"
+    };
+  }
+
+  const store = getStore("calendar");
+  const data = JSON.parse(req.body);
+
+  await store.set("events", data);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ ok: true })
+  };
+};
 
