3ed348901fde4d4eda00ef30e1eaae55cc8100d4
FGieb <79575819+FGieb@users.noreply.github.com>
2026-01-27 16:14:46 +0100

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,1 +1,29 @@
+import { getStore } from "@netlify/blobs";
+
+const store = getStore("auth");
+
+export async function createSession() {
+  const token = crypto.randomUUID();
+
+  await store.set(
+    token,
+    JSON.stringify({
+      created: Date.now(),
+    })
+  );
+
+  return token;
+}
+
+export async function isValidSession(token?: string) {
+  if (!token) return false;
+
+  const data = await store.get(token);
+  return !!data;
+}
+
+export async function destroySession(token?: string) {
+  if (!token) return;
+  await store.delete(token);
+}
 
