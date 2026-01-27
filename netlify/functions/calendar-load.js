ac3e828aa4e372cf6b50d5f360a94c1caa2b6df2
FGieb <79575819+FGieb@users.noreply.github.com>
2026-01-27 10:07:59 +0100

    Update calendar-load.js

--- netlify/functions/calendar-load.js
+++ netlify/functions/calendar-load.js
@@ -1,1 +1,15 @@
+import { getStore } from "@netlify/blobs";
+
+export default async () => {
+  const store = getStore("calendar");
+  const events = await store.get("events", { type: "json" });
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(events || {})
+  };
+};
 
